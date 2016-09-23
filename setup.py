8f5a425859ea380d0640c441c5c7f3b1acae17b4
Syrus Akbary <me@syrusakbary.com>
2016-09-22 20:52:46 -0700

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='graphene-django',
-    version='1.0.dev20160920000001',
+    version='1.0.dev20160922000001',
 
     description='Graphene Django integration',
     long_description=open('README.rst').read(),
