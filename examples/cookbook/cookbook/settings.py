60b681d70d142f92ee2386c7b35178fa84bf8111
cph <stegben.benjamin@gmail.com>
2016-10-22 14:16:06 +0800

    add graphiql support

--- examples/cookbook/cookbook/settings.py
+++ examples/cookbook/cookbook/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
+    
+    # a GUI for graphQL
+    'django_graphiql',
 
     'cookbook.ingredients.apps.IngredientsConfig',
     'cookbook.recipes.apps.RecipesConfig',
