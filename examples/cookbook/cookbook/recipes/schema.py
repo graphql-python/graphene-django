5ac046a715cdce60abdcbbb5165ef979cabb1fe1
chriscauley <chris@lablackey.com>
2016-11-03 18:06:23 -0400

    adding recipe schema

---
+++ examples/cookbook/cookbook/recipes/schema.py
@@ -0,0 +1,32 @@
+from cookbook.recipes.models import Recipe, RecipeIngredient
+from graphene import AbstractType, Node
+from graphene_django.filter import DjangoFilterConnectionField
+from graphene_django.types import DjangoObjectType
+
+class RecipeNode(DjangoObjectType):
+
+    class Meta:
+        model = Recipe
+        interfaces = (Node, )
+        filter_fields = ['title','amounts']
+        filter_order_by = ['title']
+
+class RecipeIngredientNode(DjangoObjectType):
+
+    class Meta:
+        model = RecipeIngredient
+        # Allow for some more advanced filtering here
+        interfaces = (Node, )
+        filter_fields = {
+            'ingredient__name': ['exact', 'icontains', 'istartswith'],
+            'recipe': ['exact'],
+            'recipe__name': ['icontains'],
+        }
+        filter_order_by = ['ingredient__name', 'recipe__name',]
+
+class Query(AbstractType):
+    recipe = Node.Field(RecipeNode)
+    all_recipes = DjangoFilterConnectionField(RecipeNode)
+
+    recipeingredient = Node.Field(RecipeIngredientNode)
+    all_recipeingredients = DjangoFilterConnectionField(RecipeIngredientNode)
