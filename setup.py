4246ceaa8583861e8cf660a47878aae55a803add
Syrus Akbary <me@syrusakbary.com>
2016-12-16 00:27:33 -0800

    Updated version to 1.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='graphene-django',
-    version='1.2.0',
+    version='1.2.1',
 
     description='Graphene Django integration',
     long_description=open('README.rst').read(),
@@ -33,7 +33,7 @@ setup(
 
     install_requires=[
         'six>=1.10.0',
-        'graphene>=1.1.2',
+        'graphene>=1.1.3',
         'Django>=1.6.0',
         'iso8601',
         'singledispatch>=3.4.0.3',
