32bb8c8255f43edeee4fe7d509afbbeba24065fd
Alexey Subbotin <dotsbb@users.noreply.github.com>
2017-01-23 16:19:55 +0100

    Fix unit test

--- graphene_django/tests/test_views.py
+++ graphene_django/tests/test_views.py
@@ -408,7 +408,6 @@ def test_handles_field_errors_caught_by_graphql(client):
     response = client.get(url_string(query='{thrower}'))
     assert response.status_code == 400
     assert response_json(response) == {
-        'data': None,
         'errors': [{'locations': [{'column': 2, 'line': 1}], 'message': 'Throws!'}]
     }
 
