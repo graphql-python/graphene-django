5536fc95bfd2b76643d582ec545bc33e5f0e8faa
Julien Surloppe <julien@surloppe.fr>
2017-05-02 16:45:46 +0200

    fix select_related and prefetch_related set in resolver getting lost

--- graphene_django/filter/fields.py
+++ graphene_django/filter/fields.py
@@ -62,7 +62,7 @@ class DjangoFilterConnectionField(DjangoConnectionField):
         low = default_queryset.query.low_mark or queryset.query.low_mark
         high = default_queryset.query.high_mark or queryset.query.high_mark
         default_queryset.query.clear_limits()
-        queryset = default_queryset & queryset
+        queryset &= default_queryset
         queryset.query.set_limits(low, high)
         return queryset
 
