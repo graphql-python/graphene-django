748dc4c50994c99301143aaff36035ac513309f6
Grant McConnaughey <grant@britecore.com>
2017-11-29 15:15:16 -0600

    Add id input field to model form mutation

--- graphene_django/forms/mutation.py
+++ graphene_django/forms/mutation.py
@@ -77,7 +77,7 @@ class DjangoFormMutation(BaseDjangoFormMutation):
                                     only_fields=(), exclude_fields=(), **options):
 
         if not form_class:
-            raise Exception('form_class is required for FormMutation')
+            raise Exception('form_class is required for DjangoFormMutation')
 
         form = form_class()
         input_fields = fields_for_form(form, only_fields, exclude_fields)
@@ -128,6 +128,7 @@ class DjangoModelFormMutation(BaseDjangoFormMutation):
 
         form = form_class()
         input_fields = fields_for_form(form, only_fields, exclude_fields)
+        input_fields['id'] = convert_form_field(model._meta.pk)
 
         registry = get_global_registry()
         model_type = registry.get_type_for_model(model)
