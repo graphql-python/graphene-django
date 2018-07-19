dc561c68c49d1a91637503f765857d819c36769a
Syrus Akbary <me@syrusakbary.com>
2018-07-19 16:52:06 -0700

    Updated version to 2.1.0

--- graphene_django/__init__.py
+++ graphene_django/__init__.py
@@ -1,6 +1,6 @@
 from .types import DjangoObjectType
 from .fields import DjangoConnectionField
 
-__version__ = "2.1rc1"
+__version__ = "2.1.0"
 
 __all__ = ["__version__", "DjangoObjectType", "DjangoConnectionField"]
