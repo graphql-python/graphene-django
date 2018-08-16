f8dff38e29edc644ca27eadabb5afe10757be623
Jay Hale <jay@jtst.io>
2018-08-16 14:27:30 -0400

    Remove unnecessary compat utility for Django < 1.11

--- graphene_django/compat.py
+++ graphene_django/compat.py
@@ -5,13 +5,7 @@ class MissingType(object):
 try:
     # Postgres fields are only available in Django with psycopg2 installed
     # and we cannot have psycopg2 on PyPy
-    from django.contrib.postgres.fields import ArrayField, HStoreField, RangeField
+    from django.contrib.postgres.fields import (ArrayField, HStoreField,
+                                                JSONField, RangeField)
 except ImportError:
     ArrayField, HStoreField, JSONField, RangeField = (MissingType,) * 4
-
-
-try:
-    # Postgres fields are only available in Django 1.9+
-    from django.contrib.postgres.fields import JSONField
-except ImportError:
-    JSONField = MissingType
