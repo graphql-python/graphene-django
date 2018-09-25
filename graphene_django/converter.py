979abd49e3a1ed3db2b57477ba301fd2706ccc2a
Zoltan Egyed <zoltan.egyed@vertis.com>
2018-09-25 17:48:04 +0200

    Charfield choices with blnak True are working with this

--- graphene_django/converter.py
+++ graphene_django/converter.py
@@ -71,7 +71,8 @@ def convert_django_field_with_choices(field, registry=None):
                 return named_choices_descriptions[self.name]
 
         enum = Enum(name, list(named_choices), type=EnumWithDescriptionsType)
-        converted = enum(description=field.help_text, required=not field.null)
+        required = not (field.blank or field.null or field.default)
+        converted = enum(description=field.help_text, required=required)
     else:
         converted = convert_django_field(field, registry)
     if registry is not None:
