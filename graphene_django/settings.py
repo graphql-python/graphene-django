60e6f4b7684c4ed5d58266aeeff9d26e799f52ff
Sebastian Weiss <sebastian.weiss@capgemini.com>
2018-12-29 22:56:22 +0100

    lint

--- graphene_django/settings.py
+++ graphene_django/settings.py
@@ -137,5 +137,4 @@ def reload_graphene_settings(*args, **kwargs):
     if setting == "GRAPHENE":
         graphene_settings = GrapheneSettings(value, DEFAULTS, IMPORT_STRINGS)
 
-
 setting_changed.connect(reload_graphene_settings)
