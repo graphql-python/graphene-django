ae05dee1528cbe77a51fb37e7b5dff9e87538cec
Semyon Pupkov <mail@semyonpupkov.com>
2019-06-18 23:32:49 +0500

    Make cookbook example working on django 2

--- examples/cookbook/cookbook/settings.py
+++ examples/cookbook/cookbook/settings.py
@@ -43,13 +43,12 @@ INSTALLED_APPS = [
     "cookbook.recipes.apps.RecipesConfig",
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.auth.middleware.SessionAuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
