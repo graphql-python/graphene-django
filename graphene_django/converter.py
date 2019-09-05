8909f6c7eb78ca0606597c007a0b7da63d213444
EverWinter23 <eternal.blizzard23@gmail.com>
2019-09-05 21:36:34 +0530

    converter.py: Fix typo posgres->postgres
    Fixes typo for HStoreField and RangeField
    converters.

--- graphene_django/converter.py
+++ graphene_django/converter.py
@@ -235,12 +235,12 @@ def convert_postgres_array_to_list(field, registry=None):
 
 @convert_django_field.register(HStoreField)
 @convert_django_field.register(JSONField)
-def convert_posgres_field_to_string(field, registry=None):
+def convert_postgres_field_to_string(field, registry=None):
     return JSONString(description=field.help_text, required=not field.null)
 
 
 @convert_django_field.register(RangeField)
-def convert_posgres_range_to_string(field, registry=None):
+def convert_postgres_range_to_string(field, registry=None):
     inner_type = convert_django_field(field.base_field)
     if not isinstance(inner_type, (List, NonNull)):
         inner_type = type(inner_type)
