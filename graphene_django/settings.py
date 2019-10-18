b085b5922a69f1cadfa2bb72519c19dafebb2e39
Misha K <mike.viburn@gmail.com>
2019-10-18 12:38:59 +0200

    add Django 3.0 to the test matrix (#793)
    * add Django 3.0 to the test matrix
    
    * fix six imports

--- graphene_django/settings.py
+++ graphene_django/settings.py
@@ -13,9 +13,9 @@ back to the defaults.
 """
 from __future__ import unicode_literals
 
+import six
 from django.conf import settings
 from django.test.signals import setting_changed
-from django.utils import six
 
 try:
     import importlib  # Available in Python 3.1+
