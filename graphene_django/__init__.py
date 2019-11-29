374d8a8a9e6fd2f2fdd373183f35d75229617768
Jonathan Kim <jkimbo@gmail.com>
2019-11-29 09:13:36 +0000

    v2.7.1

--- graphene_django/__init__.py
+++ graphene_django/__init__.py
@@ -1,6 +1,6 @@
 from .types import DjangoObjectType
 from .fields import DjangoConnectionField
 
-__version__ = "2.7.0"
+__version__ = "2.7.1"
 
 __all__ = ["__version__", "DjangoObjectType", "DjangoConnectionField"]
