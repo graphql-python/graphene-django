40f329cae06c6a084371c68ac8e2a4af48131947
Jonathan Kim <jkimbo@gmail.com>
2020-01-31 14:58:24 +0000

    Fix tests

--- graphene_django/tests/test_command.py
+++ graphene_django/tests/test_command.py
@@ -3,7 +3,7 @@ from mock import patch, mock_open
 from six import StringIO
 
 
-@patch("graphene_django.management.commands.graphql_schema.Command.save_file")
+@patch("graphene_django.management.commands.graphql_schema.Command.save_json_file")
 def test_generate_file_on_call_graphql_schema(savefile_mock, settings):
     out = StringIO()
     management.call_command("graphql_schema", schema="", stdout=out)
