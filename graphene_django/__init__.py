c91a7da2e7c1149f25953af148f5d8bd265f31dc
Jonathan Kim <jkimbo@gmail.com>
2020-04-13 11:55:18 +0100

    Fix formatting issue

--- graphene_django/__init__.py
+++ graphene_django/__init__.py
@@ -3,4 +3,9 @@ from .types import DjangoObjectType
 
 __version__ = "2.9.1"
 
-__all__ = ["__version__", "DjangoObjectType", "DjangoListField", "DjangoConnectionField"]
+__all__ = [
+    "__version__",
+    "DjangoObjectType",
+    "DjangoListField",
+    "DjangoConnectionField",
+]
