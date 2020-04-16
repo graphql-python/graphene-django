44679aefc530af606400ba65d22bd73b6ca5c3e9
ariel1899 <radke1899@gmail.com>
2020-04-16 14:34:13 -0400

    Fixing tests

--- graphene_django/forms/tests/test_mutation.py
+++ graphene_django/forms/tests/test_mutation.py
@@ -151,6 +151,38 @@ class FormMutationTests(TestCase):
         self.assertEqual(result.data["myMutation"]["errors"], [])
         self.assertEqual(result.data["myMutation"]["text"], "VALID_INPUT")
 
+    def test_default_meta_fields(self):
+        class MyMutation(DjangoFormMutation):
+            class Meta:
+                form_class = MyForm
+
+        self.assertNotIn("text", MyMutation._meta.fields)
+
+    def test_mirror_meta_fields(self):
+        class MyMutation(DjangoFormMutation):
+            class Meta:
+                form_class = MyForm
+                mirror_input = True
+
+        self.assertIn("text", MyMutation._meta.fields)
+
+    def test_default_input_meta_fields(self):
+        class MyMutation(DjangoFormMutation):
+            class Meta:
+                form_class = MyForm
+
+        self.assertIn("client_mutation_id", MyMutation.Input._meta.fields)
+        self.assertIn("text", MyMutation.Input._meta.fields)
+
+    def test_exclude_fields_input_meta_fields(self):
+        class MyMutation(DjangoFormMutation):
+            class Meta:
+                form_class = MyForm
+                exclude_fields = ['text']
+
+        self.assertNotIn("text", MyMutation.Input._meta.fields)
+        self.assertIn("client_mutation_id", MyMutation.Input._meta.fields)
+
 
 class ModelFormMutationTests(TestCase):
     def test_default_meta_fields(self):
@@ -314,43 +346,9 @@ class ModelFormMutationTests(TestCase):
 
         fields_w_error = {e.field: e.messages for e in result.errors}
         self.assertEqual(len(result.errors), 3)
-        self.assertIn("testCamel", fields_w_error)
-        self.assertEqual(fields_w_error['testCamel'], ["Enter a whole number."])
+        self.assertIn("test_camel", fields_w_error)
+        self.assertEqual(fields_w_error['test_camel'], ["Enter a whole number."])
         self.assertIn("name", fields_w_error)
         self.assertEqual(fields_w_error['name'], ["This field is required."])
         self.assertIn("age", fields_w_error)
         self.assertEqual(fields_w_error['age'], ["This field is required."])
-
-
-class FormMutationTests(TestCase):
-    def test_default_meta_fields(self):
-        class MyMutation(DjangoFormMutation):
-            class Meta:
-                form_class = MyForm
-
-        self.assertNotIn("text", MyMutation._meta.fields)
-
-    def test_mirror_meta_fields(self):
-        class MyMutation(DjangoFormMutation):
-            class Meta:
-                form_class = MyForm
-                mirror_input = True
-
-        self.assertIn("text", MyMutation._meta.fields)
-
-    def test_default_input_meta_fields(self):
-        class MyMutation(DjangoFormMutation):
-            class Meta:
-                form_class = MyForm
-
-        self.assertIn("client_mutation_id", MyMutation.Input._meta.fields)
-        self.assertIn("text", MyMutation.Input._meta.fields)
-
-    def test_exclude_fields_input_meta_fields(self):
-        class MyMutation(DjangoFormMutation):
-            class Meta:
-                form_class = MyForm
-                exclude_fields = ['text']
-
-        self.assertNotIn("text", MyMutation.Input._meta.fields)
-        self.assertIn("client_mutation_id", MyMutation.Input._meta.fields)
