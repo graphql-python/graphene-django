5427e2c939af31fe025c150a44f2dcf97c2c915c
ariel1899 <radke1899@gmail.com>
2020-04-17 16:00:46 -0400

    Revert "Change"
    This reverts commit c632d05a

--- graphene_django/forms/mutation.py
+++ graphene_django/forms/mutation.py
@@ -45,7 +45,13 @@ class BaseDjangoFormMutation(ClientIDMutation):
         if form.is_valid():
             return cls.perform_mutate(form, info)
         else:
-            errors = ErrorType.from_errors(form.errors)
+            errors = [
+                ErrorType(
+                    field=to_camel_case(key) if key != "__all__" else key,
+                    messages=value,
+                )
+                for key, value in form.errors.items()
+            ]
 
             return cls(errors=errors, **form.data)
 
