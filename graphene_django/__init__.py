88f6ec458cf1f2f7efdf1b583ef0f0c70cccaf61
Jonathan Kim <jkimbo@gmail.com>
2020-06-27 10:44:06 +0100

    v3.0.0b4

--- graphene_django/__init__.py
+++ graphene_django/__init__.py
@@ -1,7 +1,7 @@
 from .fields import DjangoConnectionField, DjangoListField
 from .types import DjangoObjectType
 
-__version__ = "3.0.0b3"
+__version__ = "3.0.0b4"
 
 __all__ = [
     "__version__",
