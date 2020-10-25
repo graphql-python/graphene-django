7aea0a730f9fdfe2de6ce467d68617b077219fb7
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2020-10-25 20:20:14 +0300

    Register MultipleChoiceField as list type

--- graphene_django/forms/converter.py
+++ graphene_django/forms/converter.py
@@ -63,6 +63,11 @@ def convert_form_field_to_list(field):
     return List(ID, required=field.required)
 
 
+@convert_form_field.register(forms.MultipleChoiceField)
+def convert_form_field_to_string_list(field):
+    return List(String, required=field.required)
+
+
 @convert_form_field.register(forms.DateField)
 def convert_form_field_to_date(field):
     return Date(description=field.help_text, required=field.required)
