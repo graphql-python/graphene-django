a7a8b3dca6cee0ac7e94833535fea65911b507ac
Yair Silbermintz <MisterGlass@users.noreply.github.com>
2022-02-07 09:16:41 -0500

    Replace calls to methods removed in Django v4 (#1275)
    * Replace calls to deprecated methods
    
    * Fix test config & Replace additional methods removed in django 4.0
    
    * Update tox for official Django 4 release
    
    * 2.16.0
    
    * Revert version update
    
    * Remove duplicate entry
    
    Co-authored-by: Jeremy Stretch <jstretch@ns1.com>
    
    * Limit max Django version
    
    Co-authored-by: Jeremy Stretch <jstretch@ns1.com>
    
    * Remove Python 3.5 (deprecated) from tox
    
    Co-authored-by: Jeremy Stretch <jstretch@ns1.com>
    
    Co-authored-by: Ülgen Sarıkavak <ulgens@users.noreply.github.com>
    Co-authored-by: Jeremy Stretch <jstretch@ns1.com>

--- graphene_django/tests/models.py
+++ graphene_django/tests/models.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 CHOICES = ((1, "this"), (2, _("that")))
 
--- graphene_django/tests/urls.py
+++ graphene_django/tests/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from ..views import GraphQLView
 
 urlpatterns = [
-    url(r"^graphql/batch", GraphQLView.as_view(batch=True)),
-    url(r"^graphql", GraphQLView.as_view(graphiql=True)),
+    re_path(r"^graphql/batch", GraphQLView.as_view(batch=True)),
+    re_path(r"^graphql", GraphQLView.as_view(graphiql=True)),
 ]
