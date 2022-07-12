aef30d9d0b1f141d517cbc7822336c06d56b2784
Leo(Jeemyeong) Lee <jeemyeong@gmail.com>
2022-07-12 17:37:26 +0900

    Fix: import dependency

--- graphene_django/views.py
+++ graphene_django/views.py
@@ -11,7 +11,7 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.generic import View
 from graphql import OperationType, get_operation_ast, parse, validate
 from graphql.error import GraphQLError
-from graphql.error import format_error as format_graphql_error
+from graphql.error.graphql_error import format_error as format_graphql_error
 from graphql.execution import ExecutionResult
 
 from graphene import Schema
