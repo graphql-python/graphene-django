b846c371e7875ab1c1bc5a3e18e4b23bf5aa0744
Kien Dang <mail@kien.ai>
2023-08-06 06:47:00 +0800

    Use ruff in pre-commit (#1441)
    * Use ruff in pre-commit
    
    * Add pyupgrade
    
    * Add isort
    
    * Add bugbear
    
    * Fix B015 Pointless comparison
    
    * Fix B026
    
    * B018 false positive
    
    * Remove flake8 and isort config from setup.cfg
    
    * Remove black and flake8 from dev dependencies
    
    * Update black
    
    * Show list of fixes applied with autofix on
    
    * Fix typo
    
    * Add C4 flake8-comprehensions
    
    * Add ruff to dev dependencies
    
    * Fix up

--- graphene_django/forms/converter.py
+++ graphene_django/forms/converter.py
@@ -27,8 +27,8 @@ def get_form_field_description(field):
 @singledispatch
 def convert_form_field(field):
     raise ImproperlyConfigured(
-        f"Don't know how to convert the Django form field {field} ({field.__class__}) "
-        "to Graphene type"
+        "Don't know how to convert the Django form field %s (%s) "
+        "to Graphene type" % (field, field.__class__)
     )
 
 
