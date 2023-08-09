4ac3f3f42d08fdcdd252265d59ca4c0f797a3bd1
Firas Kafri <3097061+firaskafri@users.noreply.github.com>
2023-08-10 01:12:15 +0300

    Update __init__.py

--- graphene_django/__init__.py
+++ graphene_django/__init__.py
@@ -2,7 +2,7 @@ from .fields import DjangoConnectionField, DjangoListField
 from .types import DjangoObjectType
 from .utils import bypass_get_queryset
 
-__version__ = "3.1.4"
+__version__ = "3.1.5"
 
 __all__ = [
     "__version__",
