433a7d8bcbdb0790bf7852bf7eb08e2542ea9921
Kien Dang <mail@kien.ai>
2023-09-18 17:53:04 +0800

    Fix test Client headers for Django 4.2

--- graphene_django/utils/utils.py
+++ graphene_django/utils/utils.py
@@ -1,5 +1,6 @@
 import inspect
 
+import pkg_resources
 from django.db import connection, models, transaction
 from django.db.models.manager import Manager
 from django.utils.encoding import force_str
@@ -145,3 +146,14 @@ def bypass_get_queryset(resolver):
     """
     resolver._bypass_get_queryset = True
     return resolver
+
+
+def __django_version():
+    return pkg_resources.get_distribution("django").parsed_version
+
+
+def __parse_version(v):
+    return pkg_resources.parse_version(v)
+
+
+_DJANGO_VERSION_AT_LEAST_4_2 = __django_version() >= __parse_version("4.2")
