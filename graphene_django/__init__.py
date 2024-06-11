b317bbfa1a5398e4d345493e8d278094d2dd9e43
Markus Richter <markus.richter@scale.eu>
2024-06-11 16:14:54 +0200

    Bump to 3.2.2

--- graphene_django/__init__.py
+++ graphene_django/__init__.py
@@ -2,7 +2,7 @@ from .fields import DjangoConnectionField, DjangoListField
 from .types import DjangoObjectType
 from .utils import bypass_get_queryset
 
-__version__ = "3.2.1"
+__version__ = "3.2.2"
 
 __all__ = [
     "__version__",
