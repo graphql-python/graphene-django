17889ceef4dca0e21d673e68e19bd149d3896040
Alexandre Detiste <alexandre.detiste@gmail.com>
2024-09-15 15:21:54 +0200

    remove dead code: singledispatch has been in the stard library for many years
    (BTW this function does not seems to be used anywhere anymore)

--- graphene_django/utils/utils.py
+++ graphene_django/utils/utils.py
@@ -111,25 +111,7 @@ def is_valid_django_model(model):
 
 
 def import_single_dispatch():
-    try:
-        from functools import singledispatch
-    except ImportError:
-        singledispatch = None
-
-    if not singledispatch:
-        try:
-            from singledispatch import singledispatch
-        except ImportError:
-            pass
-
-    if not singledispatch:
-        raise Exception(
-            "It seems your python version does not include "
-            "functools.singledispatch. Please install the 'singledispatch' "
-            "package. More information here: "
-            "https://pypi.python.org/pypi/singledispatch"
-        )
-
+    from functools import singledispatch
     return singledispatch
 
 
