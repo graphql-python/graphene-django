c52cf2b0458e9c3082fc30cf260a87212d1a67b5
Firas Kafri <3097061+firaskafri@users.noreply.github.com>
2025-03-13 11:29:45 +0300

    Bump version to 3.2.3

--- graphene_django/__init__.py
+++ graphene_django/__init__.py
@@ -2,7 +2,7 @@ from .fields import DjangoConnectionField, DjangoListField
 from .types import DjangoObjectType
 from .utils import bypass_get_queryset
 
-__version__ = "3.2.2"
+__version__ = "3.2.3"
 
 __all__ = [
     "__version__",
